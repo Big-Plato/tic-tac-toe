c32d1001170006077b5682d3a16e3dbc0ba2a3ed
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-06 18:39:45 -0300

    I erase everything and will do everything in the app.js file from scratch, Start to create the interface too

--- app.js
+++ app.js
@@ -1,122 +1,47 @@
 
-/* What functions will exist?
-1 - Create Player
-2 - Create Gameboard
-3 - Playround
-4 - Switch player
-5 - Print the gameBoard
-6 - Win...
-
-*/
-
+//function that selects the gameboard and use the other functions to play the game
 function GameBoard () {
-    const [rows, columns] = [3, 3];
+    const cells = document.querySelectorAll(".cell")
 
-    console.log(rows)
-    console.log(columns)
-    board = []
-    
-    // Create a 2D array
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(" ");
-        }
-    }
-    
-    //Method to get the board
+    board = Array.from(cells);
+
+    board.filter((element) => {
+        console.log(element.dataset.value)
+    })
+
+    console.log(board)
+
     const getBoard = () => board;
 
-    //Method to drop a mark in the board
-    const dropMark = (row, column, player) => {
-        const availableCells = board.filter((row) => row[column].getValue() === " ");
-        console.log(availableCells);
-
-        if (!availableCells.length) return;
-        // const play = 
-    }
-
-    const printBoard = () => {
-        console.log(getBoard())
-    }
-
-    return {getBoard, dropMark, printBoard}
+    return {getBoard, board}
 }
 
-function printGameBoard () {
-    gameBoard();
-    return console.table(board);
+// function that got the logic of the game
+function playGame () {
+    const board = GameBoard.getBoard();
+
+    console.log(board)
 }
 
-function Cell () {
-    let value = " ";
+// function to create a player based on user input
+function gamePlayer () {
+    let player = prompt("Put your name")
+    let marker = prompt("Put your mark here [X] or [O]");
 
-    const addToken = (player) => {
-        value = player.marker;
-    }
-
-    const getValue = () => value;
-
-    return {addToken, getValue};
+    let playerOne = CreatePlayer(player, marker)
+    return playerOne;
 }
 
-// Create the function to play round
-function playRound (player1, player2) {
-    let play = false;
-
-    const board = gameBoard();
-
-    console.log(board.getBoard())
-    
-} 
-
-function GameController (
-    playerOne = player1,
-    playerTwo = player2
-) {
-    const board = Gameboard();
-
-    const players = [
-        {
-            name: playerOne,
-            token: "X"
-        },
-        {
-            name: playerTwo,
-            token: "O"
-        }
-    ];
-
-    let activePlayer = players[0];
-
-    // Switch turn, if active player is [0] then switch to [1]
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
-
-    const getActivePlayer = () => activePlayer;
-
-    const printNewRound () => {
-        board.printBoard();
-    }
-}
-
-// Create Player function
-function createPlayer (name, marker) {
-    let score = 0;
+//function to create the player
+function CreatePlayer (name, marker) {
     this.name = name;
     this.marker = marker;
+
+    let score = 0;
+
     const getScore = () => score;
     const giveScore = () => score++;
 
-    return ({name, marker, getScore, giveScore});
-
+    return {name, marker, getScore, giveScore}
 }
-
-// Create two players
-const player1 = createPlayer('Player 1', 'X');
-const player2 = createPlayer('Player 2', 'O');
-
-console.log(player1);
-console.log(player2);
 
