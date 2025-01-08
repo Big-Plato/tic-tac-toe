550165cc9ebdd5270efed3c9cc3dad7dbbdc20f8
Big-Plato <luisoctavius.sc@gmail.com>
2025-01-07 23:02:47 -0300

    For Now the game is functioning in a certain way, but has problems:
    Things that I've done so far:
    1 - Implemented the logic to play the game in the console;
    2 - The computer plays, but not automatically, It still have to put the parameters in playRound functioning
    
    Still has to implement
    1 - The gameOver logic
    2 - Check for a winner
    3 - After these two steps I have to implement the UI

--- app.js
+++ app.js
@@ -1,111 +1,122 @@
-function GameBoard() {
-  const [rows, columns] = [3, 3];
-  let board = [];
 
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push(Cell());
+function GameBoard () {
+    
+    const [rows, columns] = [3, 3];
+    let board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    } 
+
+    console.log(board)
+
+    const getBoard = () => board;
+
+    const dropMark = (row, column, player) => {
+        const availableCells = board.filter((row) => row.filter((column) => column.getValue() === " "));
+
+        console.log(availableCells)
+
+        if (!availableCells.length) return;
+
+        board[row][column].addMark(player);
+    };
+
+    const printBoard = () => {
+        const boardCellsHasValues = board.map((row) => row.map((cell) => cell.getValue()));
+        console.table(boardCellsHasValues);   
     }
-  }
 
-  console.log(board);
-
-  const getBoard = () => board;
-
-  const dropMark = (row, column, player) => {
-    const availableCells = board.filter((row) =>
-      row.filter((column) => column.getValue() === " ")
-    );
-
-    console.log(availableCells);
-
-    if (!availableCells.length) return;
-
-    board[row][column].addMark(player);
-  };
-
-  const printBoard = () => {
-    const boardCellsHasValues = board.map((row) =>
-      row.map((cell) => cell.getValue())
-    );
-    console.log(boardCellsHasValues);
-  };
-
-  return { getBoard, dropMark, printBoard };
+    return {getBoard, dropMark, printBoard}
 }
 
-function Cell() {
-  let value = " ";
+ function Cell () {
+    let value = " ";
 
-  const addMark = (player) => {
-    value = player;
-  };
+    const addMark = (player) => {
+        value = player;
+    } 
 
-  const getValue = () => value;
+    const getValue = () => value;
 
-  return { addMark, getValue };
+    return {addMark, getValue}
 }
 
-function CreatePlayer(name, marker) {
-  this.name = name;
-  this.marker = marker;
+function CreatePlayer (name, marker) {
+    this.name = name;
+    this.marker = marker;
 
-  let score = 0;
-  getScore = () => score;
-  giveScore = () => score++;
+    let score = 0;
+    getScore = () => score;
+    giveScore = () => score++;
 
-  return { name, marker, getScore, giveScore };
+    return {name, marker, getScore, giveScore}
 }
 
-function GameController(playerOne, playerTwo) {
-  const board = GameBoard();
+function GameController (playerOne, playerTwo) {
+    let playerOneName = prompt("What's your name?")
+    let playerOneMark;
 
-  let playerOneName = prompt("What's your name?");
-  let playerOneMark;
+    do {
+        playerOneMark = prompt("Do you want to play with X or O?")
+    } while (playerOneMark !== 'X' && playerOneMark !== 'O');
 
-  do {
-    playerOneMark = prompt("Do you want to play with X or O?");
-  } while (playerOneMark !== "X" && playerOneMark !== "O");
+    let computerMark;
+    if (playerOneMark === 'X' ? computerMark = 'O' : computerMark = 'X');
 
-  let computerMark;
-  if (playerOneMark === "X" ? (computerMark = "O") : (computerMark = "X"));
+    playerOne = CreatePlayer(playerOneName, playerOneMark);
 
-  playerOne = CreatePlayer(playerOneName, playerOneMark);
+    playerTwo = CreatePlayer("Computer", computerMark);
 
-  playerTwo = CreatePlayer("Computer", computerMark);
+    const players = [
+        playerOne,
+        playerTwo
+    ]
 
-  const players = [playerOne, playerTwo];
+    const board = GameBoard();
 
-  console.log(players);
+    console.log(players);
 
-  let activePlayer = players[0];
+    let activePlayer = players[0];
 
-  const switchPlayerTurn = () => {
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-  };
-  const getActivePlayer = () => activePlayer;
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
 
-  const printNewRound = () => {
-    board.printBoard();
-    console.log(`${getActivePlayer().name}'s turn.`);
-  };
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`)
+    };
 
-  const playRound = (row, column) => {
-    console.log(
-      `Putting ${
-        getActivePlayer().name
-      }'s mark into row ${row} column ${column}.`
-    );
-    board.dropMark(row, column, getActivePlayer().marker);
+    console.log(players[0])
 
-    switchPlayerTurn();
-    printNewRound();
-  };
+    const playRound = (row, column) => {
+        
+                if(activePlayer === players[0]) {
+                    console.log(`Putting ${getActivePlayer().name}'s mark into row ${row} column ${column}.`);
+                    board.dropMark(row, column, getActivePlayer().marker);
+                }
+                if (activePlayer === players[1]) {
+                    row = Math.floor(Math.random() * 2);
+                    column = Math.floor(Math.random() * 2)
+                    console.log(`${getActivePlayer().name} is making a move into ${row} column ${column}.`)
+                    // do {
+                        board.dropMark(row, column, getActivePlayer().marker);
+                    // } while(board.dropMark)
+                    
+                }
+               switchPlayerTurn();
+                printNewRound(); 
+                     
+            }
+            
+            printNewRound();
 
-  printNewRound();
-
-  return { playRound, getActivePlayer };
-}
+        return {playRound, getActivePlayer};
+        }   
 
 const game = GameController();
